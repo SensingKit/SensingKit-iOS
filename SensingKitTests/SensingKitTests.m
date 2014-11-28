068b78e2534727bd299f92f7ea04af693f48cfe7
Minos Katevas <minos.kat@gmail.com>
2014-11-28 17:28:59 +0000

    Add Tests

--- SensingKitTests/SensingKitTests.m
+++ SensingKitTests/SensingKitTests.m
@@ -23,6 +23,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "SensingKitLib.h"
+#import "SKModelManager.h"
 
 @interface SensingKitTests : XCTestCase
 
@@ -42,9 +44,63 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testSensingKitLib
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    SensingKitLib *sensingKit1 = [SensingKitLib sharedSensingKitLib];
+    XCTAssertNotNil(sensingKit1, @"SensingKitLib cannot be nil");
+    
+    SensingKitLib *sensingKit2 = [SensingKitLib sharedSensingKitLib];
+    XCTAssertNotNil(sensingKit2, @"SensingKitLib cannot be nil");
+    
+    XCTAssertEqual(sensingKit1, sensingKit2, @"Instances should be identical");
+}
+
+- (void)testSensingKitRecordingDetails
+{
+    SensingKitLib *sensingKit = [SensingKitLib sharedSensingKitLib];
+    
+    NSArray *recordings = [sensingKit getRecordings];
+    XCTAssertNotNil(recordings, @"Array of RecordingDetails cannot be nil");
+    
+    XCTAssertEqual(recordings.count, 0, @"Size should be 0");
+    
+    // Recording 1
+    SKRecording *recording1 = [sensingKit newRecording];
+    XCTAssertNotNil(recording1, @"SKRecording cannot be nil");
+    XCTAssertEqual(recordings.count, 1, @"Array size should increase");
+    
+    XCTAssertEqualObjects(recording1.recordingDetails.name, @"New Recording", @"Default name should be 'New Recording'");
+    
+    recording1.recordingDetails.name = @"Recording 1";
+    XCTAssertEqualObjects(recording1.recordingDetails.name, @"Recording 1", @"Name should change to 'Recording 1'");
+    
+    SKRecordingDetails *recordingDetails1 = recordings[0];
+    XCTAssertEqualObjects(recordingDetails1.name, @"Recording 1", @"Names should be identical");
+    
+    // Recording 2
+    SKRecording *recording2 = [sensingKit newRecording];
+    XCTAssertNotNil(recording2, @"SKRecording cannot be nil");
+    XCTAssertEqual(recordings.count, 2, @"Array size should increase");
+    recording2.recordingDetails.name = @"Recording 2";
+    XCTAssertEqualObjects(recording2.recordingDetails.name, @"Recording 2", @"Name should change to 'Recording 2'");
+    XCTAssertEqualObjects(recording1.recordingDetails.name, @"Recording 1", @"Name should change to 'Recording 1'");
+    
+    // Delete Recording 1
+    [sensingKit deleteRecordingWithDetails:recordingDetails1];
+    XCTAssertEqual(recordings.count, 1, @"Array size should decrease");
+    
+    [sensingKit saveContext];
+}
+
+- (void)testModelManager
+{
+    SKModelManager *modelManager1 = [SKModelManager sharedModelManager];
+    XCTAssertNotNil(modelManager1, @"SKModelManager cannot be nil");
+    
+    SKModelManager *modelManager2 = [SKModelManager sharedModelManager];
+    XCTAssertNotNil(modelManager2, @"SKModelManager cannot be nil");
+    
+    XCTAssertEqual(modelManager1, modelManager2, @"Instances should be identical");
 }
 
 @end
