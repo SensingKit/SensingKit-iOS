e9b0ee58ad911675d34f4fb7902306abfd8d4b89
Minos Katevas <minos.kat@gmail.com>
2015-09-20 08:34:01 +0100

    Fix missing case of AuthorizationNone

--- SensingKit/SKLocation.m
+++ SensingKit/SKLocation.m
@@ -111,6 +111,10 @@
 {
     switch (authorization)
     {
+        case SKLocationAuthorizationNone:
+            // Just ignore
+            break;
+            
         case SKLocationAuthorizationWhenInUse:
             if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
                 [self.locationManager requestWhenInUseAuthorization];
