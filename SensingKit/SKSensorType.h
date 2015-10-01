c0b89361659e9904c024bcbdd4ceee2032af0217
Minos Katevas <minos.kat@gmail.com>
2015-10-01 12:28:13 +0100

    Add Documentation

--- SensingKit/SKSensorType.h
+++ SensingKit/SKSensorType.h
@@ -32,16 +32,63 @@
  */
 typedef NS_ENUM(NSUInteger, SKSensorType)
 {
+    /**
+     *  Accelerometer is a sensor that measures the device acceleration changes in three‑dimensional space. You can use this data to detect both the current orientation of the device (relative to the ground) and any instantaneous changes to that orientation.
+     */
     Accelerometer = 0,
+
+    /**
+     *  Gyroscope is a sensor that measures the device’s rate of rotation around each of the three spatial axes.
+     */
     Gyroscope,
+    
+    /**
+     *  Magnetometer (also known as Compass or Magnetic Field Sensor) is a sensor that measures the actual orientation of the device in relation to the Magnetic North.
+     */
     Magnetometer,
+    
+    /**
+     *  Device Motion sensor uses sensor fusion techniques to provide more advanced and accurate motion measurements. It measures the Attitude, Rotation Rate, Calibrated Magnetic Field, as well as a separation of User Acceleration and Gravity from the device’s acceleration.
+     */
     DeviceMotion,
+    
+    /**
+     *  Activity sensor uses an embedded motion co‑processor that senses the user’s activity classified as Stationary, Walking, Running, Automotive or Cycling.
+     */
     Activity,
+    
+    /**
+     *  Pedometer sensor uses an embedded motion co‑processor that captures pedestrian‑related data such as step counts, distance traveled and number of floors ascended or descended.
+     */
     Pedometer,
+    
+    /**
+     *  Altimeter sensor uses an embedded barometer sensor to capture changes to the relative altitude (not the actual). It also provides the recorded atmospheric pressure in kPa.
+     */
     Altimeter,
+    
+    /**
+     *  Battery sensor listens to changes in the battery charge state (Charging, Full, Unplugged) as well as in the battery charge level (with 1% precision).
+     */
     Battery,
+    
+    /**
+     *  Location sensor determines the current location of the device using a combination of Cellular, Wi‑Fi, Bluetooth and GPS sensors. It provides 2D geographical coordinate information (latitude, longitude), as well as the altitude of the device.
+     */
     Location,
+    
+    /**
+     *  iBeacon™ Proximity sensor uses Apple's iBeacon™ technology to estimate the proximity of the current device with other iBeacons in range.
+     */
     iBeaconProximity,
+    
+    /**
+     *  Eddystone™ Proximity sensor estimates the proximity of the current device with other Eddystone™ beacons in range.
+     */
     EddystoneProximity,
+    
+    /**
+     *  Microphone sensor can be used to record audio from the environment by converting sound into electrical signal.
+     */
     Microphone
 };
