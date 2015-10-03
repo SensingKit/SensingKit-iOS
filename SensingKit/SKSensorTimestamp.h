8e9a4d540b23087856f01e93e4920ced94412542
Minos Katevas <minos.kat@gmail.com>
2015-10-03 15:48:40 +0100

    Add Documentation in SKSensorTimestamp

--- SensingKit/SKSensorTimestamp.h
+++ SensingKit/SKSensorTimestamp.h
@@ -24,19 +24,54 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  SKSensorTimestamp object represent a single point in time. 
+ */
 @interface SKSensorTimestamp : NSObject <NSCopying>
 
-@property (nonatomic, readonly, copy) NSDate *timestamp;
+- (nonnull instancetype)init __attribute__((unavailable("Use [SKSensorTimestamp sensorTimestampFromDate:] or [SKSensorTimestamp sensorTimestampFromTimeInterval:] instead.")));
+
+/**
+ *  Creates and returns a new SKSensorTimestamp from an NSDate object. If the sensor does not provide an NSDate object, [NSDate date] can be used.
+ *
+ *  @param date The absolute date that this SKSensorTimestamp object should be initialized with.
+ *
+ *  @return A new SKSensorTimestamp object initialized with the (NSDate *)date parameter.
+ */
++ (nonnull instancetype)sensorTimestampFromDate:(nonnull NSDate *)date;
+
+/**
+ *  Creates and returns a new SKSensorTimestamp from a NSTimeInterval object. The time interval should be relative to the last time the device was boot. If the sensor does not provide this value, [NSProcessInfo processInfo].systemUptime can be used.
+ *
+ *  @param timeInterval The number of seconds since the last time the device was boot. Use a negative argument to specify a date and time before the reference date.
+ *
+ *  @return A new SKSensorTimestamp object initialized relative to timeInterval parameter.
+ */
++ (nonnull instancetype)sensorTimestampFromTimeInterval:(NSTimeInterval)timeInterval;
+
+/**
+ *  Returns an NSDate object of the sensor timestamp.
+ */
+@property (nonatomic, readonly, copy) NSDate * _Nonnull timestamp;
+
+/**
+ *  Returns the interval between the timestamp and the last time the device was boot.
+ */
 @property (nonatomic, readonly) NSTimeInterval timeIntervalSinceLastBoot;
 
-- (instancetype)init __attribute__((unavailable("Use [SKSensorTimestamp sensorTimestampFromDate:] or [SKSensorTimestamp sensorTimestampFromTimeInterval:] instead.")));
+/**
+ *  Returns a dictionary that encapsulates the timestamp as a string, the interval since 1970, and the interval since the device was boot.
+ */
+@property (nonatomic, readonly, copy) NSDictionary * _Nonnull timestampDictionary;
 
-+ (instancetype)sensorTimestampFromDate:(NSDate *)date;
+/**
+ *  Returns a string representation of the timestamp. The format of the string is "yyyy-MM-dd HH:mm:ss.SSS ZZZ".
+ */
+@property (nonatomic, readonly, copy) NSString * _Nonnull timestampString;
 
-+ (instancetype)sensorTimestampFromTimeInterval:(NSTimeInterval)timeInterval;
-
-@property (nonatomic, readonly, copy) NSDictionary *timestampDictionary;
-@property (nonatomic, readonly, copy) NSString *timestampString;
+/**
+ *  Returns the interval between the timestamp and 00:00:00 UTC on 1 January 1970.
+ */
 @property (nonatomic, readonly) NSTimeInterval timeIntervalSince1970;
 
 @end
