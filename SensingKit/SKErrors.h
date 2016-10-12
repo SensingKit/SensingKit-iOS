5e68fbfd2454eca9ec055fa0c8c039fa9b2eda58
Minos Katevas <minos.kat@gmail.com>
2016-10-12 16:19:17 +0200

    Remove unused import

--- SensingKit/SKErrors.h
+++ SensingKit/SKErrors.h
@@ -24,8 +24,6 @@
 
 #import <Foundation/Foundation.h>
 
-#import <Foundation/NSObject.h>
-
 static NSString *const SKErrorDomain = @"org.sensingkit.SensingKit-iOS.ErrorDomain";
 
 /* NSError codes in SKErrorDomain.
