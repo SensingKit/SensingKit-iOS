431a9b50fa68af7b626d1275a2abab020c4090c5
Kleomenis Katevas <minos.kat@gmail.com>
2021-07-20 16:50:04 +0200

    Change email in Copyright

--- SensingKit/SKHeadingConfiguration.m
+++ SensingKit/SKHeadingConfiguration.m
@@ -3,7 +3,7 @@
 //  SensingKit
 //
 //  Copyright (c) 2017. Kleomenis Katevas
-//  Kleomenis Katevas, k.katevas@imperial.ac.uk
+//  Kleomenis Katevas, minos.kat@gmail.com
 //
 //  This file is part of SensingKit-iOS library.
 //  For more information, please visit https://www.sensingkit.org
